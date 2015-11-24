ec2d1ec8f00566a68abd3b1299090726f2ba3ea1
kommissarnicko <kommissarnicko@gmail.com>
2015-11-24 00:48:28 -0700

    commenting + readme

---
+++ readme.txt
@@ -0,0 +1,9 @@
+/******************************************************************************
+** README for SMALLSH
+** NAME: Nickolas A. Williams
+** DATE: 11/23/2015
+******************************************************************************/
+
+This is a single file, so compiling should be very easy. Simply run:
+
+gcc -o smallsh smallsh.c
--- smallsh.c
+++ smallsh.c
@@ -2,7 +2,15 @@
 ** PROGRAM 3 - smallsh
 ** NAME: Nickolas A. Williams
 ** DATE: 11/23/2015
-** Shell
+** DESCRIPTION: Very simple UNIX Shell with limited functionality
+** CREDITS: Most of my reference for how this was going to work came from
+** three places:
+** 		1. Lectures 9 through 13, Benjamin Brewster
+**		   <https://oregonstate.instructure.com/courses/1555023/pages/block-3>
+**		2. Tutorial Write A Shell In C, Stephen Brennan (last accessed 11/23/2015)
+**		   <http://stephen-brennan.com/2015/01/16/write-a-shell-in-c/>
+**		3. mini-shell, by "Bharathi" (last accessed 11/23/2015)
+**		   <https://code.google.com/p/mini-shell/>
 ******************************************************************************/
 
 #include <sys/wait.h>
@@ -17,10 +25,19 @@
 #define ARG_NUMBER_LIMIT 512
 #define ARG_DELIMITER " \x0A\x09" //delimited by space, horizontal tab, newline
 
+
+/******************************************************************************
+** changeDirectory(char** argArray)
+** DESCRIPTION: changes current working directory to string in argArray[1]
+** or if null, will change directory to HOME.
+** PARAMETERS: pointer to array of char pointers (argument list); first
+** element will contain "cd", second element contains path or NULL
+** RETURNS: n/a
+******************************************************************************/
 void changeDirectory(char** argArray)
 {
 	char* directory;
-	if (argArray[1] == NULL)
+	if (argArray[1] == NULL) //user entered just "cd"
 	{
 		directory = getenv("HOME");
 	}
@@ -28,64 +45,89 @@ void changeDirectory(char** argArray)
 	{
 		directory = argArray[1];
 	}
-	// printf("directory = [%s]\n", directory);
-	// printf("final char = [%d]\n", directory[strlen(directory) - 1]);
-	if (chdir(directory) == -1)
+	if (chdir(directory) == -1) //chdir has failed, invalid directory
 	{
 		printf("Invalid directory.\n");
 	}
 }
 
 
+/******************************************************************************
+** printStatus(int status)
+** DESCRIPTION: prints to stdout status last command executed by startProcess()
+** PARAMETERS: status returned by waitpid in startProcess()
+** RETURNS: n/a
+******************************************************************************/
 void printStatus(int status)
 {
-	if ((status >= 0) && (status <= 1))
+	if ((status >= 0) && (status <= 1)) //"normal" exit
 	{
 		printf("Exit value %d\n", status);
 	}
-	if (status > 1)
+	if (status > 1) //signaled termination
 	{
 		printf("Terminated by signal %d\n", status);
 	}
 }
 
 
+/******************************************************************************
+** int executeInput(char **argArray, int status)
+** DESCRIPTION: if command is built-in, will execute. Built-in commands are:
+**				~exit: exits the shell
+**				~cd: calls changeDirectory
+**				~status: calls printStatus and passes last status
+**				~#: just a comment, doesn't do anything
+**				~NULL: if passed null, that means the user just pushed enter
+**				If not built in, will call startProcess and pass it argument list
+** PARAMETERS: argument list argArray, parsed user input, command to execute will
+** be in argArray[0].
+** RETURNS: status of executed command, either built-in or with startProcess()
+******************************************************************************/
 int executeInput(char **argArray, int status)
 {
 	if (argArray[0] == NULL)
 	{
-		return 1;
+		return 1; //user just pushed enter and did nothing
 	}
 	
 	if (strcmp("exit", argArray[0]) == 0)
 	{
-		exit(EXIT_SUCCESS);
+		exit(EXIT_SUCCESS); //exits the shell
 	}
 	
 	if (strcmp("cd", argArray[0]) == 0)
 	{
-		changeDirectory(argArray);
+		changeDirectory(argArray); //errors here are handled in function
 		return 1;
 	}
 	
 	if (strcmp("status", argArray[0]) == 0)
 	{
-		printStatus(status);
+		printStatus(status); //cannot have an error here, always success
 		return 1;
 	}
 	
 	if (strcmp("#", argArray[0]) == 0)
 	{
-		return 1;
+		return 1; //ignore input that begins with a #
 	}
 	
 	else
 	{
-		return startProcess(argArray);
+		return startProcess(argArray); //not built in, return status when done
 	}
 }
 
 
+/******************************************************************************
+** int fileInputOutput(char* term)
+** DESCRIPTION: checks out argArray[1] to see if it's a redirection for
+** input/output for use in startProcess()
+** PARAMETERS: string of chars from argArray[1], either a < > or neither
+** RETURNS: 0 means neither < nor >; 1 = > (pipe output of command in argArray[0] 
+** to file), or 2 = < (pipe output of command in argArray[0] to file)
+******************************************************************************/
 int fileInputOutput(char* term)
 {
 	if (strcmp(">", term) == 0)
@@ -96,20 +138,32 @@ int fileInputOutput(char* term)
 	{
 		return 2;
 	}
-	else
+	else //no applicable item in argArray[1]
 	{
 		return 0;
 	}
 }
 
 
+/******************************************************************************
+** int startProcess(char **argArray)
+** DESCRIPTION: Given array of arguments, checks if file I/O operations are
+** needed, if so, opens or creates file, then executes program stored in
+** first argument space
+** PARAMETERS: array of pointers to strings; argArray[0] = command, 
+** argArray[1] = could be < or > for file I/O, argArray[2]... optional
+** RETURNS: integer returned by waitpid once child process terminates
+** CREDITS: The file I/O portion of this is derived directly from Lecture 12
+** "Pipes and IPC" by Benjamin Brewster,
+** <https://oregonstate.instructure.com/courses/1555023/pages/block-3>
+******************************************************************************/
 int startProcess(char **argArray)
 {
 	pid_t pid;
 	int status;
 	int fd, fd2;
 	int redirect = 0;
-	if (argArray[1] != NULL)
+	if (argArray[1] != NULL) //prevents against segfaults in single-arg commands
 	{
 		redirect = fileInputOutput(argArray[1]);
 	}
@@ -119,6 +173,7 @@ int startProcess(char **argArray)
 	{
 		if (redirect == 1) //second item in argArray is >
 		{
+			//file I/O from lectures
 			fd = open(argArray[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 			if (fd == -1)
 			{
@@ -139,6 +194,7 @@ int startProcess(char **argArray)
 		}
 		else if (redirect == 2) //second item in argArray is <
 		{
+			//file I/O from lectures
 			fd = open(argArray[2], O_RDONLY);
 			if (fd == -1)
 			{
@@ -159,6 +215,7 @@ int startProcess(char **argArray)
 		}
 		else //second item in argArray is not < or >
 		{
+			//non file-I/O command
 			if (execvp(argArray[0], argArray) == -1)
 			{
 				perror("smallsh: ");
@@ -166,11 +223,11 @@ int startProcess(char **argArray)
 			exit(EXIT_FAILURE);
 		}
 	}
-	else if (pid < 0)
+	else if (pid < 0) //error!
 	{
 		perror("smallsh: ");
 	}
-	else
+	else //shell will wait for child to complete process
 	{
 		do
 		{
@@ -178,10 +235,16 @@ int startProcess(char **argArray)
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 	
-	return status;
+	return status; //for use by status command
 }
 
 
+/******************************************************************************
+** char* readInput()
+** DESCRIPTION: reads input from stdin
+** PARAMETERS: n/a
+** RETURNS: array containing raw data from stdin
+******************************************************************************/
 char* readInput()
 {
 	size_t bufferSize = BUFFER_CHAR_LIMIT;
@@ -198,6 +261,14 @@ char* readInput()
 }
 
 
+/******************************************************************************
+** char** parseInput(char *input)
+** DESCRIPTION: separates input from readInput() into an array of strings for
+** use as a list of arguments
+** PARAMETERS: raw array of chars from readInput()
+** RETURNS: array of pointers to arrays of chars (strings), each string
+** is NULL-terminated
+******************************************************************************/
 char** parseInput(char *input)
 {
 	size_t argLimit = ARG_NUMBER_LIMIT;
@@ -223,6 +294,13 @@ char** parseInput(char *input)
 }
 
 
+/******************************************************************************
+** void promptLoop()
+** DESCRIPTION: main loop for performing shell operations: gets user input,
+** parses user input, executes user input, holds status of execution
+** PARAMETERS: N/A
+** RETURNS: N/A
+******************************************************************************/
 void promptLoop()
 {
 	char *input;
@@ -242,6 +320,9 @@ void promptLoop()
 }
 
 
+/******************************************************************************
+** MAIN!
+******************************************************************************/
 int main()
 {
 	promptLoop();
